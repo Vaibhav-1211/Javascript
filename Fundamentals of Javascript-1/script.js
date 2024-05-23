f5eccd0a474f4f42e90aa3f2a826e30f65af88c8
Vaibhav Kabira <vaibhavkabira995@gmail.com>
2024-05-23 23:36:43 +0530

    files updated

--- Fundamentals of Javascript-1/script.js
+++ Fundamentals of Javascript-1/script.js
@@ -7,14 +7,14 @@ console.log(24)
 let firstName = 'Vaibhav'
 console.log(firstName);
 
-//Variable name cannot start with number 
+//Variable name cannot start with number
 //$ symbol is allowed to use as variable name in js
 let $function = 'FuntionSir'
 //name is stil resereved keyword but it allows and still leagal to use
 let name = 'Hoeny Singh'
 //We should not start variable name with uppercase letter
 let person = 'Kevin'
-//Variable that all are in uppercase are resevered for  constants that are never change like 
+//Variable that all are in uppercase are resevered for  constants that are never change like
 let PI = 3.1415;
 //Final one that variable naem must be descriptive
 let myFirstJob = "Programmer"
@@ -67,7 +67,7 @@ console.log(typeof null);
 // const birthYear = 2000;
 // // birthYear = 1990;
 
-// //It should work we get error 
+// //It should work we get error
 // //It immutable varible in other words it varible which cannot be changed
 
 // //const job;
@@ -101,7 +101,7 @@ console.log(typeof null);
 // //Assingment opearators
 // let c = 10+5;
 // c += 10; //+= means   c = c +10
-// c *= 10; 
+// c *= 10;
 // c++ ;// c = c + 1
 // c--; // c = c -1
 // console.log(c);
@@ -109,16 +109,77 @@ console.log(typeof null);
 // //Comparison Operators: to produce boolean values
 // console.log(vaibhavAge > swayamAge); //> ,<,>=,<=
 // console.log(vaibhavAge >= 18);
-// const isFullAge = vaibhavAge >= 18; 
+// const isFullAge = vaibhavAge >= 18;
 
 //Operator precedence
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(now - 1991 > now - 2018);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(now - 1991 > now - 2018);
 
-let x, y
-x = y = 25 - 10 - 5; //x = y = 10 x = 10
-console.log(x,y); 
- 
+// let x, y
+// x = y = 25 - 10 - 5; //x = y = 10 x = 10
+// console.log(x,y);
 
+//Practice
+
+//BMI = mass / (height * height)
+
+
+//BMI = mass / (height * height)
+
+// const  massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark)
+
+// const BMIJohn = massJohn / (heightJohn * heightJohn)
+
+// console.log(BMIMark)
+// console.log(BMIJohn)
+
+// const markHigherBMI = true;
+// console.log(BMIMark > BMIJohn)
+
+//5. Strings and Template Literals
+// const firstName =  'Jonas';
+// const job = 'Teacher';
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas = 'I am' + firstName + ',a' + (year - birthYear) + 'year old' + job + '!';
+// console.log(jonas);
+// const jonasNew = `I'm ${firstName}`;
+// console.log(jonasNew)
+
+// console.log(`Just a regular string`)
+// console.log(`String with \n\  multiple \n\ lines`)//to use in new multiple lines or multiline string
+
+//6. Taking Decision: IF else statements //Control Structure
+// const age = 13;
+// // const isOldEnough = ;
+// if (age >= 18) {
+//   console.log('Sarah can start driving 🚗')
+// } else {
+//   console.log('Sarah cannot drive');
+// }
+
+// //dynamic variable
+// const birthYear = 1998;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21
+// }
+
+// console.log(century)
+
+//7. Type Conversion and Coercion
+//Type conversion means when we manually convert type to another where as type coercion is when js automatically converts type bts for us.
+
+const inputYear = '1991';
+console.log(Number(inputYear))
+console.log(inputYear + 18);// to fix this we can use inbuilt js Number() fn.
