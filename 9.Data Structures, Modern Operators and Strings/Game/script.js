8e6bed417987381b3a54836deead57910b2b29ed
Vaibhav Kabira <vaibhavkabira995@gmail.com>
2024-06-27 21:52:27 +0530

    commited games

--- 9.Data Structures, Modern Operators and Strings/Game/script.js
+++ 9.Data Structures, Modern Operators and Strings/Game/script.js
@@ -232,6 +232,41 @@ const game = {
 //It can has any type
 
 const rest = new Map();
-rest.set('name','Classic Italiano');
-rest.set(1,'Dimond,Itely');
-console.log(rest.set(2,'Lisbon, Portugal'));
+rest.set('name', 'Classic Italiano');
+//here, set method is pretty similar to add method that i used in previous Set() method
+rest.set(1, 'Dimond,Itely');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')//boolean keys true and false
+  .set(false, 'We are closed :( ')
+
+//getting values from the set()
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+
+console.log(rest.has('categories'));
+rest.delete(2); console.log(rest);
+const arr = [1, 2];
+rest.set(arr, 'Test')
+console.log(rest);
+
+
+//maps also has the size property
+console.log(rest.size);
+
+//clear() property to clear everything on map
+// rest.clear();
+
+//We can use arrays or object as map keys.
+// rest.set([1,2],'Test')
+console.log(rest.get(arr));
+
