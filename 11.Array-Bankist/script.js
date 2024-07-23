5589fb319df546026fde89c6b5084737fead1efa
Vaibhav Kabira <vaibhavkabira995@gmail.com>
2024-07-23 13:11:48 +0530

    simple array method

--- 11.Array-Bankist/script.js
+++ 11.Array-Bankist/script.js
@@ -74,3 +74,53 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//---->Simple Array Methods
+//So basically, they are functions attached to objects.
+// So if we have array methods, that means that arrays themselves are also objects. And so these array methods are simply functions that are attached to all arrays that we create in JavaScript.
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//Slice method - with the slice method we can extract any part of the array but without changing the original array.
+//BEGIN PARAMETER
+// console.log(arr.slice(2));//it return new array or copy of array with extracted part.
+
+// //END PARAMETER
+// console.log(arr.slice(2, 4));//
+
+// //NEGATIVE BEGIN PARAMETER
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// //It is easiest way to get last element of any array.
+
+// //We can use slice method to simply create shallow copy of any array.
+// console.log(arr.slice());
+// //with spread also we can do same.
+// console.log([...arr]);
+
+//-------->SPLICE METHOD
+// console.log(arr.splice(2));
+//
+//delete count
+// arr.splice(-1)//deleting last element form an array similar to slice method
+// arr.splice(1,2)
+// console.log(arr);
+
+
+//---->Reverse method
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);//it also mutate the orginal array or change the array
+
+// //--->CONCAT method
+// const letters = arr.concat(arr2)//concat method
+// console.log(letters);
+// console.log([...arr, ...arr2]);//concat method by using spread operator
+
+// //----->JOIN method
+// console.log(letters.join(' | '));
+
+//Other methods like push(),pop(),shift(),unshift(),indexof(),includes()
+
+//------>The new at Method
